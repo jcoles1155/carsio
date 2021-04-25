0ebcb49917619787af664b9a3e4f16f200273f24
JC Coles <jcoles1155@gmail.com>
2021-04-24 18:01:54 -0700

    initial setup

---
+++ main_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
