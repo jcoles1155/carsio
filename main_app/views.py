8b5707c40e1e5667f2aaeead67e676a280aa3322
JC Coles <jcoles1155@gmail.com>
2021-04-27 21:31:35 -0700

    added a details page

--- main_app/views.py
+++ main_app/views.py
@@ -29,3 +29,7 @@ def about(request):
 def cars_index(request):
     cars = CarPost.objects.all()
     return render(request, 'cars/index.html', { 'cars': cars })
+
+def cars_detail(request, car_id):
+    car = CarPost.objects.get(id=car_id)
+    return render(request, 'cars/detail.html', { 'car': car })
