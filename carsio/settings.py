4934eb2b3155290d358a97cf1fd40de2019232ea
TuckTuckC <mrkittay847@gmail.com>
2021-04-28 21:53:05 -0400

    added decouple

--- carsio/settings.py
+++ carsio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-df%#p1)x3xh7x-_(@#^rd*#k=3_x6t((&2g)+)qol@59i)tzyn'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
